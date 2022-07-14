82b174c7bf77686e97e31543eb9caf3a8c26c49f
Doniyor <doniyorcoach@gmail.com>
2022-07-15 04:19:41 +0500

    Add code

---
+++ pr15_2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace pr15_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ButtonName = new System.Windows.Forms.Label();
+            this.ButtonDesc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.buttons_MouseLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseLeave += new System.EventHandler(this.buttons_MouseLeave);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseLeave += new System.EventHandler(this.buttons_MouseLeave);
+            this.button3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button3_MouseMove);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseLeave += new System.EventHandler(this.buttons_MouseLeave);
+            this.button4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button4_MouseMove);
+            // 
+            // ButtonName
+            // 
+            resources.ApplyResources(this.ButtonName, "ButtonName");
+            this.ButtonName.Name = "ButtonName";
+            // 
+            // ButtonDesc
+            // 
+            resources.ApplyResources(this.ButtonDesc, "ButtonDesc");
+            this.ButtonDesc.Name = "ButtonDesc";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ButtonDesc);
+            this.Controls.Add(this.ButtonName);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label ButtonName;
+        private System.Windows.Forms.Label ButtonDesc;
+    }
+}
+
---
+++ pr15_2/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace pr15_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int counter = 0;
+        private bool pressI = false;
+        private bool pressX = false;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (button1.Location.Y > 4)
+            {
+                button1.Top -= 10;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (ModifierKeys == Keys.Shift)
+            {
+                button3.Width -= 3;
+                button3.Height -= 3;
+            }
+            else if (ModifierKeys == Keys.Control)
+            {
+                if (counter == 0)
+                {
+                    button3.Font = new Font("Times", 10, FontStyle.Bold);
+                    counter++;
+                }
+
+                else if (counter == 1)
+                {
+                    button3.Font = new Font("Times", 10, FontStyle.Italic);
+                    counter++;
+                }
+                else if (counter == 2)
+                {
+                    button3.Font = new Font("Times", 10, FontStyle.Underline);
+                    counter++;
+                }
+                else if (counter == 3)
+                {
+                    button3.Font = new Font("Times", 10, FontStyle.Regular);
+                    counter -= 3;
+                }
+            }
+            else
+            {
+                button3.Width += 3;
+                button3.Height += 3;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.ControlBox = !ControlBox;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (ModifierKeys == Keys.Alt && pressI) 
+                {
+                    button1.Top = button2.Top;
+                    button2.Enabled = false;
+                    button1.Enabled = true;
+                    button3.Width = 88;
+                    button3.Height = 34;
+                    button3.Font = new Font("Times", 10, FontStyle.Regular);
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.I)
+                pressI = true;
+            if (e.KeyCode == Keys.X)
+            {
+                pressX = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.I)
+                pressI = false;
+            if (e.KeyCode == Keys.X)
+            {
+                pressX = false;
+            }
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (ModifierKeys == Keys.Alt && pressX)
+                {
+                    this.Close();
+                }
+            }
+        }
+
+        private void buttons_MouseLeave(object sender, EventArgs e)
+        {
+            ButtonName.Text = "";
+            ButtonDesc.Text = "";
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            ButtonName.Text = "Кнопка 1";
+            ButtonDesc.Text = "прячет / показывает 2-ю;";
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            ButtonName.Text = "Кнопка 2";
+            ButtonDesc.Text = "сдвигает первую кнопку на 10 пикселей вверх;";
+        }
+
+        private void button3_MouseMove(object sender, MouseEventArgs e)
+        {
+            ButtonName.Text = "Кнопка 3";
+            ButtonDesc.Text = $"(1-сама по себе, 2 - с Shift, 3 - c Ctrl){Environment.NewLine} 1-увеличивает, 2-уменьшает, 3-переключает по кругу (из 3-х) шрифты на форме;";
+        }
+
+        private void button4_MouseMove(object sender, MouseEventArgs e)
+        {
+            ButtonName.Text = "Кнопка 4";
+            ButtonDesc.Text = "вкл./выкл. системную кнопку;";
+        }
+    }
+}
